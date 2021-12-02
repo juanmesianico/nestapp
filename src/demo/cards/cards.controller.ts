a5391d3833e6e8be5d06465e2ef0285a335d8fed
Juan Mesa <upsnoexisten@gmail.com>
2021-12-02 08:06:20 -0500

    bug fixed

--- src/demo/cards/cards.controller.ts
+++ src/demo/cards/cards.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Req } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 
 @Controller('cards')
 export class CardsController {
 
+    @Get()
     getIndex(@Req() resquest): string{
         return "This are my cards";
     }
